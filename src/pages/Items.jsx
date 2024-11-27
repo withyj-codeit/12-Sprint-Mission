435343d3adb1d920fc35af5d2fa9fc86251eadf5
withyj-codeit <withyj@codeit.com>
2024-11-28 07:49:11 +0900

    feat: Pagination 제거

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -1,50 +1,24 @@
 import './Items.css';
 import { useEffect, useState } from 'react';
-import { Pagination, usePagination } from '../components/Pagination';
 import { CardList } from '../components/CardList'
-
-const PAGE_SIZE = 10;
-const PAGE_COUNT = 5;
 
 export const Items = () => {
   const [ data, setData ] = useState([]);
   const pageSize = 10;
-  const pageCount = 5;
-  const { 
-    currentPage,
-    startPage,
-    totalPageCount,
-    onPrevClick,
-    onNextClick,
-    onPageClick
-   } = usePagination({
-    totalCount: data.totalCount,
-    pageSize: PAGE_SIZE,
-    pageCount: PAGE_COUNT,
-  })
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(`https://panda-market-api.vercel.app/products?page=${currentPage}&pageSize=${pageSize}&orderBy=recent`);
+      const response = await fetch(`https://panda-market-api.vercel.app/products?page=1&pageSize=${pageSize}&orderBy=recent`);
       const data = await response.json();
       setData(data);
     }
     fetchData();
-  }, [currentPage]);
+  }, [pageSize]);
 
   return (
     <div className='container'>
-      <h1>6주차 - 2</h1>
+      <h1>7주차 - 2</h1>
       <CardList list={data.list} />
-      <Pagination
-        currentPage={currentPage}
-        startPage={startPage}
-        pageCount={pageCount}
-        totalPageCount={totalPageCount}
-        onPrevClick={onPrevClick}
-        onNextClick={onNextClick}
-        onPageClick={onPageClick}
-      />
     </div>
   )
 }
