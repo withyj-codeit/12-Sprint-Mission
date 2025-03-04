76e5f2953c20fd20e1ee43e570f4c7d8804003f7
withyj-codeit <withyj@codeit.com>
2025-03-05 02:50:05 +0900

    fix: queryClient 오류 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient()
+const queryClient = new QueryClient()
 
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
