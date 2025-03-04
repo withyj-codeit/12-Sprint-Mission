a6a991131fbb3a8e206656460347f5ed5112e1d1
withyj-codeit <withyj@codeit.com>
2025-03-05 02:51:38 +0900

    feat(shared): baseURL 수정

--- src/shared/utils/axiosInstance.ts
+++ src/shared/utils/axiosInstance.ts
@@ -1,5 +1,8 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-  baseURL: "https://winereview-api.vercel.app/12-1",
+  baseURL: "https://winereview-api.vercel.app/12-3",
+  // headers: {
+  //   Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTExMSwidGVhbUlkIjoiMTItMzMiLCJzY29wZSI6ImFjY2VzcyIsImlhdCI6MTc0MDczMTkyMSwiZXhwIjoxNzQwNzMzNzIxLCJpc3MiOiJzcC1lcGlncmFtIn0.mPtMTFGet2FZ-YZQlfxcNwawlcFOOwvr2O5s_vQEbN0`,
+  // },
 })
